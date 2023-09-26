ae38767ccd4950750317e2820a08e259aa4520f6
BanditosTM <banditostm123@gmail.com>
2023-09-26 17:45:08 +0300

    Added configuration and connection of routers

--- src/main.js
+++ src/main.js
@@ -1,17 +1,24 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import folderRouter from './routers/folder.js'
+import taskRouter from './routers/task.js'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT
 
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 mongoose.connect(process.env.MONGODB_URL).catch(error => console.log(error))
 
-app.get('/', (req, res) => {
-	res.send('Hello Express JS')
-})
+app.use('/api', folderRouter)
+app.use('/api', taskRouter)
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}...`)
